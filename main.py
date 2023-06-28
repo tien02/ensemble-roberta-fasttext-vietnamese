842d6db22fe6d658c32acce90996bb2532fe94f0
Tien Dang <tiendang@Tiens-MacBook-Pro.local>
2023-06-29 03:39:46 +0700

    edit training

--- main.py
+++ main.py
@@ -1,36 +1,40 @@
+import os
 import yaml
 import argparse
-import torch
 from pytorch_lightning import seed_everything
 from utils import load_data, load_model, load_trainer
 
 def parse_opt():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--task', type=str, required=True, action='store', choices=['train', 'test'], default='train', help="Perform the choosen task, 'train' for training, 'test' for evaluate on test set")
-    parser.add_argument('--data', type=str, required=True, action='store', help="Path to DATA configuration .yaml")
-    parser.add_argument('--model', type=str, required=True, action='store', help="Path to MODEL configuration .yaml")
-    parser.add_argument('--trainer', type=str, required=True, action='store', help="Path to TRAINER configuration .yaml")
-    opt = parser.parse_args()
-    return opt
+        parser = argparse.ArgumentParser()
+        parser.add_argument('--task', type=str, action='store', choices=['train', 'test'], default='train', help="Perform the choosen task, 'train' for training, 'test' for evaluate on test set")
+        parser.add_argument('--data', type=str, action='store', default='./config/data.yaml', help="Path to DATA configuration .yaml")
+        parser.add_argument('--model', type=str, action='store', default='./config/model.yaml', help="Path to MODEL configuration .yaml")
+        parser.add_argument('--trainer', type=str, action='store', default='./config/trainer.yaml', help="Path to TRAINER configuration .yaml")
+        opt = parser.parse_args()
+        return opt
 
 def run(task:str, data:str, model:str, trainer:str):
         with open(trainer) as f:
                 trainer_config = yaml.safe_load(f)
 
         seed_everything(trainer_config['seed'])
-        
-        model_module = load_model(model)
-        model_module = torch.compile(model_module)
-        dm = load_data(data)
 
         trainer_module = load_trainer(trainer)
+        
+        dm = load_data(data)
+
         if task == "train":
+                dm.setup(stage='fit')
+                model_module = load_model(model,loss_weight=dm.train_data.class_weights)
+                
                 trainer_module.fit(model=model_module, datamodule=dm, ckpt_path=trainer_config['keep_training_path'])
         if task == "test":
-               if trainer_config['test_ckpt'] is None:
-                      print("Please specify the test_ckpt")
-                      return
-               trainer_module.test(model=model_module, datamodule=dm, ckpt_path=trainer_config['test_ckpt'])
+                dm.setup(stage='test')
+                model_module = load_model(model)
+                if trainer_config['test_ckpt'] is None:
+                        print("Please specify the test_ckpt")
+                        return
+                trainer_module.test(model=model_module, datamodule=dm, ckpt_path=trainer_config['test_ckpt'])
 
 def main(opt):
        run(**vars(opt))
--- utils.py
+++ utils.py
@@ -1,26 +1,35 @@
+import os
 import yaml
 
 from src.models import *
 from src.dataset import DataModule
 from src.trainers import PhoBERTModel, FastTextLSTMModel
 
-from pytorch_lightning import Trainer, seed_everything
-from pytorch_lightning.callbacks import ModelCheckpoint, EarlyStopping
-from pytorch_lightning.loggers import TensorBoardLogger
+from lightning.pytorch import Trainer, seed_everything
+from lightning.pytorch.callbacks import ModelCheckpoint, EarlyStopping
+from lightning.pytorch.loggers import TensorBoardLogger
 
 def load_data(path_to_yaml_file:str):
+    if not os.path.exists(path_to_yaml_file):
+        print(f"Can't find {path_to_yaml_file}")
+        return
+    
     with open(path_to_yaml_file) as f:
         data_config = yaml.safe_load(f)
-    dm = DataModule(root_data_dir=data_config['root_data_dir'], 
-                    label=data_config['label'], 
+
+    dm = DataModule(root_data_dir=data_config['root_data_dir'],  
                     model_type=data_config['model_type'], 
                     batch_size=data_config['batch_size'], 
                     num_workers=data_config['num_workers'], 
                     fasttext_embedding=data_config['fasttext_embedding'])
+
     return dm
     
 
-def load_model(path_to_yaml_file:str):
+def load_model(path_to_yaml_file:str, loss_weight=None):
+    if not os.path.exists(path_to_yaml_file):
+        print(f"Can't find {path_to_yaml_file}")
+        return
     with open(path_to_yaml_file) as f:
         model_config = yaml.safe_load(f)
 
@@ -53,21 +62,32 @@ def load_model(path_to_yaml_file:str):
     
     # system configuration
     if name.startswith("FASTTEXT"):
-        system = FastTextLSTMModel(dropout=model_config['drop_out'])
+        system = FastTextLSTMModel(dropout=model_config['drop_out'], 
+                                   out_channels=model_config['out_channels'],
+                                   hidden_size=model_config['vector_size'],
+                                   loss_weight=loss_weight)
     else:
-        system = PhoBERTModel(model=model)
+        system = PhoBERTModel(model=model, 
+                              out_channels=model_config['out_channels'],
+                              loss_weight=loss_weight)
     
     return system
 
 
 def load_trainer(path_to_yaml_file:str):
+    if not os.path.exists(path_to_yaml_file):
+        print(f"Can't find {path_to_yaml_file}")
+        return
+    
     with open(path_to_yaml_file) as f:
         trainer_config = yaml.safe_load(f)
 
     checkpoint_callback = ModelCheckpoint(dirpath=trainer_config['ckpt_dir'], monitor="val_loss", save_top_k=3, mode="min")
     early_stopping = EarlyStopping(monitor="val_loss", mode="min")
 
-    logger = TensorBoardLogger(save_dir=trainer_config['tensorboard']['dir'], name=trainer_config['tensorboard']['name'], version=trainer_config['tensorboard']['version'])
+    logger = TensorBoardLogger(save_dir=trainer_config['tensorboard']['dir'], 
+                               name=trainer_config['tensorboard']['name'], 
+                               version=trainer_config['tensorboard']['version'])
 
     trainer = Trainer(accelerator=trainer_config['accelarator'], check_val_every_n_epoch=trainer_config['val_each_epoch'],
                     gradient_clip_val=1.0,max_epochs=trainer_config['max_epochs'],
